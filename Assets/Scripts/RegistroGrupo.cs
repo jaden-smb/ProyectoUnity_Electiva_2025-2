2915fe8473d714a99406d96595f5f784e7a9ff35
Santiago Montoya <santiagomontoyabaiter@gmail.com>
2025-08-06 13:47:22 -0500

    revert

--- Assets/Scripts/RegistroGrupo.cs
+++
@@ -1,9 +0,0 @@
-//	Nombre completo: Santiago Montoya Baiter
-//	Fecha: 26/07/2025	
-//	Hora: 10:43 PM	
-//	Nombre completo: Fernando Barbosa Hernandez
-//	Fecha: 27/07/2025	
-//	Hora: 6:11 PM	
-//	Nombre completo: Angie Natalia Montaño Ballesteros
-//	Fecha: 27/07/2025	
-//	Hora: 6:13 PM	
